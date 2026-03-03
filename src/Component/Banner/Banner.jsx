44906939bfc9454ed1589386d64fa58d18ebce8a
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-03 20:32:38 +0600

    added

--- src/Component/Banner/Banner.jsx
+++ src/Component/Banner/Banner.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Banner1 from "../../assets/vector1.png"
 
 const Banner = () => {
     return (
         <div>
-            Banner...
+           <div className="card bg-base-100 w-96 shadow-sm">
+                <figure>
+                    <img
+                    // className='#422AD5'
+                    src={Banner1}
+                    alt="Shoes" />
+                </figure>
+            </div>
         </div>
     );
 };
--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -5,24 +5,34 @@ const Footer = () => {
         <div>
             <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
                 <nav>
-                    <h6 className="footer-title">Services</h6>
-                    <a className="link link-hover">Branding</a>
-                    <a className="link link-hover">Design</a>
-                    <a className="link link-hover">Marketing</a>
-                    <a className="link link-hover">Advertisement</a>
+                    <h3 className="font-bold text-lg">CS — Ticket System</h3>
+                    <p>Providing reliable and efficient <br />
+                     ticketing solutions for seamless <br /> customer support and issue resolution.</p>
                 </nav>
                 <nav>
                     <h6 className="footer-title">Company</h6>
                     <a className="link link-hover">About us</a>
-                    <a className="link link-hover">Contact</a>
-                    <a className="link link-hover">Jobs</a>
-                    <a className="link link-hover">Press kit</a>
+                    <a className="link link-hover">Our Mission</a>
+                    <a className="link link-hover">Contact Saled</a>
                 </nav>
                 <nav>
-                    <h6 className="footer-title">Legal</h6>
-                    <a className="link link-hover">Terms of use</a>
+                    <h6 className="footer-title">Services</h6>
+                    <a className="link link-hover">Product & Services</a>
+                    <a className="link link-hover">Customer Stories</a>
+                    <a className="link link-hover">Download Apps</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Information</h6>
                     <a className="link link-hover">Privacy policy</a>
-                    <a className="link link-hover">Cookie policy</a>
+                    <a className="link link-hover">Terms & Conditions</a>
+                    <a className="link link-hover">Join Us</a>
+                </nav>
+                 <nav>
+                    <h6 className="footer-title">Social Links</h6>
+                    <a className="link link-hover">Facebook</a>
+                    <a className="link link-hover">Twitter</a>
+                    <a className="link link-hover">Instagram</a>
+                    <a className="link link-hover">LinkedIn</a>
                 </nav>
             </footer>
         </div>
