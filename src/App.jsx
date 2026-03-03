5d4dd3baf7fd03a9a2b432eebba901b24272467f
mojahidmamu <abdullahallmojahidstudent@gmail.com>
2026-03-03 20:21:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import Banner from './Component/Banner/Banner'
+import Footer from './Component/Footer/Footer'
+import Main from './Component/Main/Main'
+import Navbar from './Component/Navbar/Navbar'
+import './index.css'
+
+function App() { 
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Banner></Banner>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <div>
+            main..part...in here..
+        </div>
+    );
+};
+
+export default Main;
